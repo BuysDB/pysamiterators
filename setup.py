fcc093d584588ab75ebbc006b8d02319e06a0e7d
buysdb <i@buysdb.nl>
2019-05-15 15:58:27 +0200

    Setup update

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ setup(
 	name='pysamiterators',
 	version='0.01',
 	description='Pysam related iterators',
-	url='https://github.com/BuysDB/pysamiterators'
+	url='https://github.com/BuysDB/pysamiterators',
+	packages=['pysamiterators',]
 )
-
