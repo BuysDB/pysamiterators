c0db956ed1e6d9e4742b5ae9534835b95cd3267a
buysdb <i@buysdb.nl>
2019-05-15 16:09:36 +0200

    Comma

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
 	name='pysamiterators',
 	version='0.01',
-	author='Buys de Barbanson'
+	author='Buys de Barbanson',
 	author_email='code@buysdb.nl',
 	description='Pysam related iterators',
 	url='https://github.com/BuysDB/pysamiterators',
