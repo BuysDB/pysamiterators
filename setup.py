7ffed51c66df00db9ad40aec0d18fb7f78534f4d
buysdb <i@buysdb.nl>
2019-05-15 16:09:02 +0200

    Added author information

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 setup(
 	name='pysamiterators',
 	version='0.01',
+	author='Buys de Barbanson'
+	author_email='code@buysdb.nl',
 	description='Pysam related iterators',
 	url='https://github.com/BuysDB/pysamiterators',
 	packages=['pysamiterators',]
