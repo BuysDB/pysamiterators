0e256e5176642babda618501729c210c6d7d07cc
BuysDB <git@buysdb.nl>
2019-07-16 14:51:48 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup
 setup(
 	name='pysamiterators',
-	version='0.01',
+	version='1.0',
 	author='Buys de Barbanson',
 	author_email='code@buysdb.nl',
 	description='Pysam related iterators',
 	url='https://github.com/BuysDB/pysamiterators',
+	download_url='https://github.com/BuysDB/pysamiterators/archive/v1.0.tar.gz',
 	packages=['pysamiterators'
         ],
-	install_requires=['pysam']
+	install_requires=['pysam'],
+	 keywords = ['mate-pair','pysam','iteration'],
+	license='MIT',
+	classifiers=[
+	 'Development Status :: 4 - Beta',
+	  'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+	  'License :: OSI Approved :: MIT License'
+	]
 )
